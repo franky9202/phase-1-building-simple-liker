70c755871b63bf182fe9078e0251a25bfc69c3ed
Edgar <franky9202@gmail.com>
2022-05-08 20:47:34 -0700

    Completed Assignment

--- main.js
+++ main.js
@@ -3,10 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hidden = document.querySelector('#modal')
+hidden.classList.add('hidden')
 
-
-
-
+const hearts = document.querySelectorAll(".like-glyph")
+for (const heart of hearts){
+  heart.addEventListener('click', () => {
+    if (heart.textContent === EMPTY_HEART)
+      mimicServerCall()
+      .then(() => {
+        heart.classList.add('.activated-heart')
+        heart.textContent = FULL_HEART
+      })
+      .catch(error => {
+        setTimeout( () => {
+          hidden.classList.add('hidden')
+          hidden.textContent = error.message
+        }, 3000)
+        hidden.classList.remove('hidden')
+        
+      })
+    if (heart.textContent === FULL_HEART){
+      heart.classList.remove('.activated-heart')
+        heart.textContent = EMPTY_HEART
+    }
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
